5fc5086a11b4fd352c4bfad56645009a2123a3ba
Sven Helmberger <fforw@gmx.de>
2021-11-11 22:41:24 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import domready from "domready"
+import "./style.css"
+
+const PHI = (1 + Math.sqrt(5)) / 2;
+const TAU = Math.PI * 2;
+const DEG2RAD_FACTOR = TAU / 360;
+
+const config = {
+    width: 0,
+    height: 0
+};
+
+/**
+ * @type CanvasRenderingContext2D
+ */
+let ctx;
+let canvas;
+
+domready(
+    () => {
+
+        canvas = document.getElementById("screen");
+        ctx = canvas.getContext("2d");
+
+        const width = (window.innerWidth) | 0;
+        const height = (window.innerHeight) | 0;
+
+        config.width = width;
+        config.height = height;
+
+        canvas.width = width;
+        canvas.height = height;
+
+        ctx.fillStyle = "#000";
+        ctx.fillRect(0,0, width, height);
+    }
+);
